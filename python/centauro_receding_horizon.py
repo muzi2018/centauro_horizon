43e9e556d6d810eb01dd141703fa59df403093f0
Weijie Wang <duangsir518@163.com>
2024-10-18 10:16:56 +0200

    syn

--- python/centauro_receding_horizon.py
+++ python/centauro_receding_horizon.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 import rospy
 import rospkg
-
-import cartesian_interface.roscpp_utils as roscpp
+import casadi as cs
 
 from centauro_horizon.msg import WBTrajectory
 from std_msgs.msg import Float64
-
-
+from horizon.problem import Problem
+from xbot_interface import config_options as co
+from xbot_interface import xbot_interface as xbot
 rospy.init_node('centauro_walk_srbd')
-roscpp.init('centauro_walk_srbd', [])
 
 solution_publisher = rospy.Publisher('/mpc_solution', WBTrajectory, queue_size=1, tcp_nodelay=True)
 solution_time_publisher = rospy.Publisher('/mpc_solution_time', Float64, queue_size=1, tcp_nodelay=True)
@@ -35,6 +34,101 @@ dt = T / ns
 
 prb = Problem(ns, receding=True, casadi_type=cs.SX)
 prb.setDt(dt)
+
+'''
+Build ModelInterface and RobotStatePublisher
+'''
+cfg = co.ConfigOptions()
+cfg.set_urdf(urdf)
+cfg.set_srdf(srdf)
+cfg.generate_jidmap()
+cfg.set_string_parameter('model_type', 'RBDL')
+cfg.set_string_parameter('framework', 'ROS')
+cfg.set_bool_parameter('is_model_floating_base', True)
+
+'''
+open/closed loop
+'''
+closed_loop = rospy.get_param(param_name='~closed_loop', default=False)
+
+'''
+xbot
+'''
+xbot_param = rospy.get_param(param_name="~xbot", default=False)
+
+base_pose = None
+base_twist = None
+# est = None
+robot = None
+
+
+if xbot_param:
+    robot = xbot.RobotInterface(cfg)
+    robot.sense()
+
+    if not closed_loop:
+        rospy.Subscriber('/xbotcore/imu/imu_link', Imu, imu_callback)
+        while base_pose is None:
+            rospy.sleep(0.01)
+
+        base_pose[0:3] = [0.07, 0., 0.8]
+        base_twist = np.zeros(6)
+    else:
+        # rospy.Subscriber('/xbotcore/link_state/pelvis/pose', PoseStamped, gt_pose_callback)
+        # rospy.Subscriber('/xbotcore/link_state/pelvis/twist', TwistStamped, gt_twist_callback)
+        rospy.Subscriber('/centauro_base_estimation/base_link/pose', PoseStamped, gt_pose_callback)
+        rospy.Subscriber('/centauro_base_estimation/base_link/twist', TwistStamped, gt_twist_callback)
+
+        while base_pose is None or base_twist is None:
+            rospy.sleep(0.01)
+
+    q_init = robot.getJointPosition()
+    q_init = robot.eigenToMap(q_init)
+
+else:
+    print('RobotInterface not created')
+
+    q_init = {
+        # 'torso_yaw': 0.00,  # 0.00,
+        # 'j_arm1_1': 1.50,  # 1.60,
+        # 'j_arm1_2': 0.1,  # 0.,
+        # 'j_arm1_3': 0.2,  # 1.5,
+        # 'j_arm1_4': -2.2,  # 0.3,
+        # 'j_arm1_5': 0.00,  # 0.00,
+        # 'j_arm1_6': -1.3,  # 0.,
+        # 'j_arm1_7': 0.0,    # 0.0,
+
+        # 'j_arm2_1': 1.50,  # 1.60,
+        # 'j_arm2_2': 0.1,  # 0.,
+        # 'j_arm2_3': -0.2,  # 1.5,
+        # 'j_arm2_4': -2.2,  # -0.3,
+        # 'j_arm2_5': 0.0,  # 0.0,
+        # 'j_arm2_6': -1.3,  # 0.,
+        # 'j_arm2_7': 0.0,    # 0.0,
+
+        # 'd435_head_joint': 0.0,
+        # 'velodyne_joint': 0.0,
+        'hip_yaw_1': -0.746,
+        'hip_pitch_1': -1.254,
+        'knee_pitch_1': -1.555,
+        'ankle_pitch_1': -0.3,
+
+        'hip_yaw_2': 0.746,
+        'hip_pitch_2': 1.254,
+        'knee_pitch_2': 1.555,
+        'ankle_pitch_2': 0.3,
+
+        'hip_yaw_3': 0.746,
+        'hip_pitch_3': 1.254,
+        'knee_pitch_3': 1.555,
+        'ankle_pitch_3': 0.3,
+
+        'hip_yaw_4': -0.746,
+        'hip_pitch_4': -1.254,
+        'knee_pitch_4': -1.555,
+        'ankle_pitch_4': -0.3,
+    }
+
 
 rate = rospy.Rate( 100 )
 while not rospy.is_shutdown():
