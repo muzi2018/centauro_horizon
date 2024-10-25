b07ec6828eea728ddccfc7f614e1e6b32cd95911
Weijie Wang <duangsir518@163.com>
2024-10-25 13:30:54 +0200

    syn

--- python/centauro_receding_horizon.py
+++ python/centauro_receding_horizon.py
@@ -406,7 +406,7 @@ for c in model.cmap.keys():
 
 short_stance_duration = 5
 stance_duration = 15
-flight_duration = 30
+flight_duration = 15
 c_i = 0
 # print("pm type is " , type(pm.getTimelines()))
 # print("Timelines : ",pm.getTimelines()['contact_1_timeline'])
