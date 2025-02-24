c391f0b4fab9339a39d726bd6256855c52bc1bde
Weijie Wang <duangsir518@163.com>
2025-02-24 15:40:17 +0100

    funtion
    #description
    
    camera work well

--- python/centauro_camera.py
+++ python/centauro_camera.py
@@ -23,9 +23,6 @@ import matplotlib.pyplot as plt
 from ultralytics import YOLO  # Import YOLO model from the ultralytics package
 
 
-print("ok")
-
-exit()
 bridge = CvBridge()
 
 # Load YOLO model
@@ -52,6 +49,7 @@ def depth_callback(msg):
     global depth_frame
     try:
         depth_frame = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")  # Depth in millimeters
+        print("get the depth_frame")
     except Exception as e:
         print(f"Error converting depth image: {e}")
 
@@ -71,6 +69,7 @@ def get_depth_at(x, y):
         
         
 def image_callback(msg):
+    print("Converted image successfully")
     try:
         frame = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
         frame = frame.copy() 
@@ -144,141 +143,19 @@ def image_callback(msg):
 
 
 
-def imu_callback(msg: Imu):
-    global base_pose
-    base_pose = np.zeros(7)
-    base_pose[3:] = np.array([msg.orientation.x, msg.orientation.y, msg.orientation.z, msg.orientation.w])
-
-def gt_twist_callback(msg):
-    global base_twist
-    base_twist = np.array([msg.twist.linear.x, msg.twist.linear.y, msg.twist.linear.z,
-                           msg.twist.angular.x, msg.twist.angular.y, msg.twist.angular.z])
-    
-    
-def gt_pose_callback(msg):
-    global base_pose
-    base_pose = np.array([msg.pose.position.x, msg.pose.position.y, msg.pose.position.z,
-                          msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z,
-                          msg.pose.orientation.w])
-    
-
-
-
-rospy.init_node('centauro_walk_srbd')
+rospy.init_node('centauro_camera')
 rospy.sleep(1.)    
-'''
-Load urdf and srdf
-'''
-urdf = rospy.get_param(param_name='/robot_description', default='')
-if urdf == '':
-    raise print('urdf not set')
-srdf = rospy.get_param(param_name='/robot_description_semantic', default='')
-if srdf == '':
-    raise print('srdf not set')
-file_dir = rospkg.RosPack().get_path('centauro_horizon')
 
-rate = rospy.Rate(50)
+rate = rospy.Rate(10)
 
-'''
-Build ModelInterface and RobotStatePublisher
-'''
-cfg = co.ConfigOptions()
-cfg.set_urdf(urdf)
-cfg.set_srdf(srdf)
-cfg.generate_jidmap()
-cfg.set_string_parameter('model_type', 'RBDL')
-cfg.set_string_parameter('framework', 'ROS')
-cfg.set_bool_parameter('is_model_floating_base', True)
 
-'''
-xbot
-'''
-xbot_param = rospy.get_param(param_name="~xbot", default=False)
-
-'''
-open/closed loop
-'''
-closed_loop = rospy.get_param(param_name='~closed_loop', default=False)
-
-if xbot_param:
-    print ("xbot i on")
-    # exit()
-    robot = xbot.RobotInterface(cfg)
-    model_fk = robot.model()
-    robot.sense()
-
-    if not closed_loop:
-        rospy.Subscriber('/xbotcore/imu/imu_link', Imu, imu_callback)
-        while base_pose is None:
-            rospy.sleep(0.01)
-
-        base_pose[0:3] = [0.07, 0., 0.8]
-        base_twist = np.zeros(6)
-    else:
-        # rospy.Subscriber('/xbotcore/link_state/pelvis/pose', PoseStamped, gt_pose_callback)
-        # rospy.Subscriber('/xbotcore/link_state/pelvis/twist', TwistStamped, gt_twist_callback)
-        rospy.Subscriber('/centauro_base_estimation/base_link/pose', PoseStamped, gt_pose_callback)
-        rospy.Subscriber('/centauro_base_estimation/base_link/twist', TwistStamped, gt_twist_callback)
-
-        while base_pose is None or base_twist is None:
-            rospy.sleep(0.01)
-
-    q_init = robot.getJointPosition()
-    q_init = robot.eigenToMap(q_init)
-
-else:
-    print('RobotInterface not created')
-
-    q_init = {
-        # 'torso_yaw': 0.00,  # 0.00,
-        # 'j_arm1_1': 1.50,  # 1.60,
-        # 'j_arm1_2': 0.1,  # 0.,
-        # 'j_arm1_3': 0.2,  # 1.5,
-        # 'j_arm1_4': -2.2,  # 0.3,
-        # 'j_arm1_5': 0.00,  # 0.00,
-        # 'j_arm1_6': -1.3,  # 0.,
-        # 'j_arm1_7': 0.0,    # 0.0,
-
-        # 'j_arm2_1': 1.50,  # 1.60,
-        # 'j_arm2_2': 0.1,  # 0.,
-        # 'j_arm2_3': -0.2,  # 1.5,
-        # 'j_arm2_4': -2.2,  # -0.3,
-        # 'j_arm2_5': 0.0,  # 0.0,
-        # 'j_arm2_6': -1.3,  # 0.,
-        # 'j_arm2_7': 0.0,    # 0.0,
-
-        # 'd435_head_joint': 0.0,
-        # 'velodyne_joint': 0.0,
-        'hip_yaw_1': -0.746,
-        'hip_pitch_1': -1.254,
-        'knee_pitch_1': -1.555,
-        'ankle_pitch_1': -0.3,
-
-        'hip_yaw_2': 0.746,
-        'hip_pitch_2': 1.254,
-        'knee_pitch_2': 1.555,
-        'ankle_pitch_2': 0.3,
-
-        'hip_yaw_3': 0.746,
-        'hip_pitch_3': 1.254,
-        'knee_pitch_3': 1.555,
-        'ankle_pitch_3': 0.3,
-
-        'hip_yaw_4': -0.746,
-        'hip_pitch_4': -1.254,
-        'knee_pitch_4': -1.555,
-        'ankle_pitch_4': -0.3,
-    }
-    base_pose = np.array([0.07, 0., 0.8, 0., 0., 0., 1.])
-    base_twist = np.zeros(6)
-
-rospy.Subscriber("/D435_head_camera/color/image_raw", Image, image_callback) 
-rospy.Subscriber("/D435_head_camera/aligned_depth_to_color/image_raw", Image, depth_callback) 
+rospy.Subscriber("/D435i_camera/color/image_raw", Image, image_callback) 
+# rospy.Subscriber("/D435_camera/aligned_depth_to_color/image_raw", Image, depth_callback) 
 
 while not rospy.is_shutdown():
-    # print('perception')
+    print('perception2')
+
     rate.sleep()
-    # rospy.spin()
 
-
+rospy.spin()
 
