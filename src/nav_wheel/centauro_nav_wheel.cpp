e87a3dd5cec0f4e0bd85a1d6b9a504ebb2776f5e
Weijie Wang <duangsir518@163.com>
2025-02-26 13:10:34 +0100

    nav_wheel not work

--- src/nav_wheel/centauro_nav_wheel.cpp
+++ src/nav_wheel/centauro_nav_wheel.cpp
@@ -20,55 +20,110 @@
 #include <std_srvs/Empty.h>
 #include <xbot_msgs/JointCommand.h>
 #include <std_msgs/Bool.h>
-
+bool start_nav_bool = false;
 void printMessage(const std::string& message) {
   std::cout << message << std::endl;
 }
 
-int main() {
+bool start_nav(std_srvs::Empty::Request& req, std_srvs::Empty::Response& res)
+{
+    start_nav_bool = !start_nav_bool;
+    return true;
+};
 
-    // const std::string robotName = "centauro";
-    // // Initialize ros node
-    // ros::init(argc, argv, robotName);
-    // ros::NodeHandle nodeHandle("");
+int main(int argc, char **argv)
+{
 
-    // std_srvs::Empty srv;
-    // // Create a Buffer and a TransformListener
-    // tf2_ros::Buffer tfBuffer;
-    // tf2_ros::TransformListener tfListener(tfBuffer);
+    const double dt = 0.1; double time_ = 0.0 ;
+    const std::string robotName = "centauro";
+    // Initialize ros node
+    ros::init(argc, argv, robotName);
+    ros::NodeHandle nodeHandle("");
 
-    // auto cfg = XBot::ConfigOptionsFromParamServer();
-    // // and we can make the model class
-    // auto model = XBot::ModelInterface::getModel(cfg);
-    // auto robot = XBot::RobotInterface::getRobot(cfg);
-    // // initialize to a homing configuration
-    // Eigen::VectorXd qhome;
-    // model->getRobotState("home", qhome);
-    // qhome[44] = 0.33;
-    // model->setJointPosition(qhome);
-    // model->update();
-    // XBot::Cartesian::Utils::RobotStatePublisher rspub (model);
-    // robot->setControlMode(
-    //     {
-    //         {"j_wheel_1", XBot::ControlMode::Velocity()},
-    //         {"j_wheel_2", XBot::ControlMode::Velocity()},
-    //         {"j_wheel_3", XBot::ControlMode::Velocity()},
-    //         {"j_wheel_4", XBot::ControlMode::Velocity()}
-    //     }
-    // );
+    std_srvs::Empty srv;
+    // Create a Buffer and a TransformListener
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener(tfBuffer);
 
-    // auto ctx = std::make_shared<XBot::Cartesian::Context>(
-    //             std::make_shared<XBot::Cartesian::Parameters>(dt),
-    //             model
-    //         );
+    auto cfg = XBot::ConfigOptionsFromParamServer();
+    // and we can make the model class
+    auto model = XBot::ModelInterface::getModel(cfg);
+    auto robot = XBot::RobotInterface::getRobot(cfg);
+    // initialize to a homing configuration
+    Eigen::VectorXd qhome;
+    model->getRobotState("home", qhome);
+    qhome[44] = 0.33;
+    model->setJointPosition(qhome);
+    model->update();
+    XBot::Cartesian::Utils::RobotStatePublisher rspub (model);
+    robot->setControlMode(
+        {
+            {"j_wheel_1", XBot::ControlMode::Velocity()},
+            {"j_wheel_2", XBot::ControlMode::Velocity()},
+            {"j_wheel_3", XBot::ControlMode::Velocity()},
+            {"j_wheel_4", XBot::ControlMode::Velocity()}
+        }
+    );
 
-    // // load the ik problem given a yaml file
-    // std::string problem_description_string;
-    // nodeHandle.getParam("problem_description_wheel", problem_description_string);
-    // auto ik_pb_yaml = YAML::Load(problem_description_string);
-    // XBot::Cartesian::ProblemDescription ik_pb(ik_pb_yaml, ctx);
+    auto ctx = std::make_shared<XBot::Cartesian::Context>(
+                std::make_shared<XBot::Cartesian::Parameters>(dt),
+                model
+            );
 
+    // load the ik problem given a yaml file
+    std::string problem_description_string;
+    nodeHandle.getParam("problem_description_wheel", problem_description_string);
+    auto ik_pb_yaml = YAML::Load(problem_description_string);
+    XBot::Cartesian::ProblemDescription ik_pb(ik_pb_yaml, ctx);
 
-    printMessage("Hello, world!");
+    auto solver = XBot::Cartesian::CartesianInterfaceImpl::MakeInstance("OpenSot",
+                                                       ik_pb, ctx
+                                                       );
+    ros::ServiceServer service = nodeHandle.advertiseService("start_nav", start_nav);
+    ros::Rate r(10);
+
+    double roll_e, pitch_e, yaw_e;
+    double K_x = 0.1, K_y = 0.2, K_yaw = 0.1;
+    Eigen::VectorXd q, qdot, qddot;
+    auto car_task = solver->getTask("base_link");
+    auto car_cartesian = std::dynamic_pointer_cast<XBot::Cartesian::CartesianTask>(car_task);
+    Eigen::Vector6d E;
+    while (ros::ok())
+    {
+        double x_e = 1;
+        double y_e = 2 ;
+        tf2::Quaternion q_;
+        q_.setW(0);
+        q_.setX(0);
+        q_.setY(0);
+        q_.setZ(1);
+        tf2::Matrix3x3 m(q_);
+        m.getRPY(roll_e, pitch_e, yaw_e);
+
+        E[0] = K_x * x_e;
+        E[1] = K_y * y_e;
+        E[2] = 0;
+        E[3] = 0;
+        E[4] = 0;
+        E[5] = K_yaw * 0;
+        car_cartesian->setVelocityReference(E);
+
+        solver->update(time_, dt);
+        model->getJointPosition(q);
+        model->getJointVelocity(qdot);
+        model->getJointAcceleration(qddot);
+        q += dt * qdot + 0.5 * std::pow(dt, 2) * qddot;
+        qdot += dt * qddot;
+        model->setJointPosition(q);
+        model->setJointVelocity(qdot);
+        model->update();
+        robot->setPositionReference(q.tail(robot->getJointNum()));
+        robot->setVelocityReference(qdot.tail(robot->getJointNum()));
+        robot->move();
+        time_ += dt;
+        rspub.publishTransforms(ros::Time::now(), "");
+    }
+        ros::spinOnce();
+        r.sleep();
     return 0;
 }
